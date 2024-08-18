0c0e2430f4bd7920226945ab76648894349b7780
Graham Wetzler <graham@wetzler.dev>
2024-08-18 10:45:49 -0500

    wezterm config

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,67 @@
+local wezterm = require 'wezterm'
+local act = wezterm.action
+
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("BlexMono Nerd Font")
+config.font_size = 14
+
+config.color_scheme = 'Dracula'
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.80
+config.macos_window_background_blur = 10
+
+config.keys = {
+  {
+    key = 'k',
+    mods = 'CMD',
+    action = act.Multiple {
+      act.ClearScrollback 'ScrollbackAndViewport',
+      act.SendKey { key = 'L', mods = 'CTRL' },
+    },
+  },
+  {
+    key = '"',
+    mods = 'LEADER',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = '%',
+    mods = 'LEADER',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'j', -- or DownArrow
+    mods = 'LEADER',
+    action = wezterm.action.ActivatePaneDirection('Down'),
+  },
+  {
+    key = 'k', -- or UpArrow
+    mods = 'LEADER',
+    action = wezterm.action.ActivatePaneDirection('Up'),
+  },
+  {
+    key = 'x',
+    mods = 'LEADER',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+  {
+    key = 'h', -- or LeftArrow
+    mods = 'LEADER',
+    action = wezterm.action.ActivatePaneDirection('Left'),
+  },
+  {
+    key = 'l', -- or RightArrow
+    mods = 'LEADER',
+    action = wezterm.action.ActivatePaneDirection('Right'),
+  },
+}
+
+config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
+
+config.disable_default_key_bindings = false
+
+return config
